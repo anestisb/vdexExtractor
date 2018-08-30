6c0b540319fb5a066c1ffa1094822c2b6fef2f15
Anestis Bechtsoudis <anestis@census-labs.com>
2018-08-30 13:17:48 +0300

    Fix bug when handling files with empty QuickenData
    Signed-off-by: Anestis Bechtsoudis <anestis@census-labs.com>

--- src/vdex/vdex_019.c
+++ src/vdex/vdex_019.c
@@ -132,6 +132,7 @@ void vdex_019_GetQuickeningInfo(const u1 *cursor, vdex_data_array_t *pQuickInfo)
   } else {
     pQuickInfo->data = NULL;
     pQuickInfo->offset = -1;
+    pQuickInfo->size = 0;
   }
 }
 
@@ -238,7 +239,7 @@ bool vdex_019_SanityCheck(const u1 *cursor, size_t bufSz) {
   // Check that verifier deps section doesn't point past the end of file
   vdex_data_array_t vDeps;
   vdex_019_GetVerifierDeps(cursor, &vDeps);
-  if (vDeps.offset + vDeps.size > bufSz) {
+  if (vDeps.size > 0 && ((vDeps.offset + vDeps.size) > bufSz)) {
     LOGMSG(l_ERROR, "Verifier dependencies section points past the end of file (%" PRIx32
                     " + %" PRIx32 " > %" PRIx32 ")",
            vDeps.offset, vDeps.size, bufSz);
@@ -248,7 +249,7 @@ bool vdex_019_SanityCheck(const u1 *cursor, size_t bufSz) {
   // Check that quickening info section doesn't point past the end of file
   vdex_data_array_t quickInfo;
   vdex_019_GetQuickeningInfo(cursor, &quickInfo);
-  if (quickInfo.offset + quickInfo.size > bufSz) {
+  if (quickInfo.size > 0 && ((quickInfo.offset + quickInfo.size) > bufSz)) {
     LOGMSG(l_ERROR, "Quickening info section points past the end of file (%" PRIx32 " + %" PRIx32
                     " > %" PRIx32 ")",
            quickInfo.offset, quickInfo.size, bufSz);
